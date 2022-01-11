f2e348779db06d4b64015ccd547453c92f4a65c7
Pragath <pragathrao11@gmail.com>
2022-01-11 22:53:53 +0530

    ProductDetails

--- src/Components/Product/ImageBox.jsx
+++ src/Components/Product/ImageBox.jsx
@@ -3,31 +3,40 @@ import { styled } from '@mui/styles'
 import React from 'react'
 
 const LeftContainer = {
-    padding: "40px 0px 0px 80px"
+    padding: "40px auto",
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center"
 
 }
 
 const Image = {
     padding: "15px 20px",
     border: "1px solid #f0f0f0",
-    width: "90%"
+    width: "85%"
 }
 
 
 function ImageBox({ image }) {
 
-    const ActionButton = styled(Button)`
-    width: 46%;
-    border-radius: 2px;
-    height: 50px;
-    background:${(props) => (props.Background)};
-    color: ${(props) => props.Color}
-`
+
+    const ActionButton = styled(Button)({
+        width: "40%",
+        borderRadius: "2px",
+        height: "50px",
+        background: (props) => `${(props.Background)}`,
+        color: (props) => `${(props.Color)}`,
+        margin: " 10px 20px 0px 0px"
+    })
+
+
     return (
         <Box sx={LeftContainer}>
             <img src={image} style={Image} />
-
-
+            <Box sx={{ display: "flex", justifyContent: "center", width: "100%" }}>
+                <ActionButton Background="#ff9f00" Color="White" variant="contained">Add to Cart</ActionButton>
+                <ActionButton Background="#fb641b" Color="White" variant="contained">Buy Now</ActionButton>
+            </Box>
         </Box>
     )
 }
